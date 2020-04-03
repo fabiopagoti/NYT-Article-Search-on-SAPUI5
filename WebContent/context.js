8267ae91406ef71c978aefa853a3e07887750113
Fabio Pagoti <fabiopagoti@gmail.com>
2020-04-02 21:09:56 -0300

    new api key

--- WebContent/context.js
+++ WebContent/context.js
@@ -17,7 +17,7 @@ NYT_Article_Search_app = function() {
 /*
  * References to APIs and models
  */
-NYT_Article_Search_app.url_generator = new NYT.article.URL_Generator("9753c12656e96dca754d7b875494dc9c:14:68901588"); // api-key,
+NYT_Article_Search_app.url_generator = new NYT.article.URL_Generator("bn4Oil58pGPpRsEUPs24SBDGO5GH8Bqi"); // api-key,
 NYT_Article_Search_app.articlesModel = new sap.ui.model.json.JSONModel(NYT_Article_Search_app.article_url);
 
 NYT_Article_Search_app.article_url = function() {
